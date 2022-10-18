434b6d64222456e11aef726b0d4d13ce5de9d88d
tp9222 <tp9222@gmail.com>
2022-10-18 14:00:33 +0530

    Update namp_normal.py

--- Terminal_Screenshot/nmap/namp_normal.py
+++ Terminal_Screenshot/nmap/namp_normal.py
@@ -2,17 +2,31 @@ import pyscreenshot
 import os
 import colorama
 from colorama import Fore
+from wakepy import set_keepawake, unset_keepawake
+
 
 with open('ip.txt') as f:
     while True:
         line = f.readline()
         if not line:
             break
+        # To prevent screen from going to sleep
+        set_keepawake(keep_screen_awake=False)
         # To capture the screen
         os.system("clear")
+
+        #Print title in red color
         print(Fore.RED + "Scan Running on IP Address: "+line)
+        
+        #Print output in white color
         print(Fore.WHITE)
-        os.system("nmap -p- -Pn -n -A -sC -sV --script vuln -oA "+line +" "+line)
+        
+        #remove /n from target line
+        line=line.strip()
+
+        command=("nmap -p- -Pn -n -A -sC -sV --script vuln "+line +" -oA "+ line)
+        print(command)
+        os.system(command)
         image = pyscreenshot.grab()
 
         # To display the captured screenshot
@@ -20,4 +34,8 @@ with open('ip.txt') as f:
 
         # To save the screenshot
         image.save(line, 'png')
+
+        #release sleeplock
+        unset_keepawake()
+        
         print(line.strip())
