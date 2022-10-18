185a17ae8c02aa819af2849aa1a1584694728d1e
tp9222 <tp9222@gmail.com>
2022-10-18 11:36:53 +0530

    Create SSH_Terminal_Screenshot.py

---
+++ Terminal_Screenshot/SSH_Scan/SSH_Terminal_Screenshot.py
@@ -0,0 +1,23 @@
+import pyscreenshot
+import os
+import colorama
+from colorama import Fore
+
+with open('ip.txt') as f:
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        # To capture the screen
+        os.system("clear")
+        print(Fore.RED + "Scan Running on IP Address: "+line)
+        print(Fore.WHITE)
+        os.system("python sshscan.py -t"+" "+line )
+        image = pyscreenshot.grab()
+
+        # To display the captured screenshot
+        #image.show()
+
+        # To save the screenshot
+        image.save(line, 'png')
+        print(line.strip())
