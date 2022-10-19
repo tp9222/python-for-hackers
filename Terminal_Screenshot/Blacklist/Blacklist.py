86106d684b810d9788605b3901c039c8f1fa0a18
tp9222 <tp9222@gmail.com>
2022-10-19 11:00:07 +0530

    Update Blacklist.py

--- Terminal_Screenshot/Blacklist/Blacklist.py
+++ Terminal_Screenshot/Blacklist/Blacklist.py
@@ -1,4 +1,30 @@
+
+import pyscreenshot
+import os
+import colorama
+from colorama import Fore
 import pydnsbl
-ip_checker = pydnsbl.DNSBLIpChecker()
-RESULT=ip_checker.check('68.128.212.240')
-print(RESULT.blacklisted)
+
+with open('ip.txt') as f:
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        # To capture the screen
+        os.system("clear")
+        print(Fore.RED + "Scan Running on IP Address: "+line)
+        print(Fore.WHITE)
+        ip_checker = pydnsbl.DNSBLIpChecker()
+        RESULT=ip_checker.check(line)
+        image = pyscreenshot.grab()
+        if (RESULT.blacklisted)
+          print(RESULT.blacklisted)
+          print(RESULT.detected_by)
+
+        # To display the captured screenshot
+        #image.show()
+
+        # To save the screenshot
+        image.save(line, 'png')
+        print(line.strip())
+
