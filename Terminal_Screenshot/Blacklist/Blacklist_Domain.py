79c35198c4c8f108896c8a84a0820402b4f95cc5
tp9222 <tp9222@gmail.com>
2022-10-19 12:02:55 +0530

    Create Blacklist_Domain.py

---
+++ Terminal_Screenshot/Blacklist/Blacklist_Domain.py
@@ -0,0 +1,50 @@
+import pyscreenshot #screenshots
+import os # OS commands
+import colorama # color output
+from colorama import Fore # color output
+import pydnsbl # black list idenfification
+import json 
+
+blacklisted_list={}
+
+with open('ip.txt') as f:
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        # To capture the screen
+        os.system("clear")
+        
+        #display current IP in red title
+
+        print(Fore.RED + "Scan Running on IP Address: "+line)
+        print(Fore.WHITE)
+
+        #Remove newline character
+
+        line=line.strip()
+        
+        #blacklist validation
+
+        ip_checker = pydnsbl.DNSBLIpChecker()
+        RESULT=ip_checker.check(line)
+        image = pyscreenshot.grab()
+
+        #if IP blacklisted then write to file
+
+        if (RESULT.blacklisted):
+          print(RESULT.blacklisted)
+          print(RESULT.detected_by)
+          blacklisted_list=(RESULT.detected_by)
+          write_to_file = open("blacklist.txt", "a")
+          write_to_file.writelines("\nblacklisted IP: "+line)
+          write_to_file.write("\nBlacklisted By:")
+          write_to_file.write(json.dumps(blacklisted_list))
+          write_to_file.writelines("\n------------------------")
+          write_to_file.close()
+        # To display the captured screenshot
+        #image.show()
+
+        # To save the screenshot
+        image.save(line, 'png')
+        print(line.strip())
