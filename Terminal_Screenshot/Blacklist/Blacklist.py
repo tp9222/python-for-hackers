329204f7ced5a24812786079763897e22e7c5b70
tp9222 <tp9222@gmail.com>
2022-10-19 12:01:46 +0530

    Update Blacklist.py

--- Terminal_Screenshot/Blacklist/Blacklist.py
+++ Terminal_Screenshot/Blacklist/Blacklist.py
@@ -1,9 +1,11 @@
+import pyscreenshot #screenshots
+import os # OS commands
+import colorama # color output
+from colorama import Fore # color output
+import pydnsbl # black list idenfification
+import json 
 
-import pyscreenshot
-import os
-import colorama
-from colorama import Fore
-import pydnsbl
+blacklisted_list={}
 
 with open('ip.txt') as f:
     while True:
@@ -12,20 +14,37 @@ with open('ip.txt') as f:
             break
         # To capture the screen
         os.system("clear")
+        
+        #display current IP in red title
+
         print(Fore.RED + "Scan Running on IP Address: "+line)
         print(Fore.WHITE)
+
+        #Remove newline character
+
         line=line.strip()
+        
+        #blacklist validation
+
         ip_checker = pydnsbl.DNSBLIpChecker()
         RESULT=ip_checker.check(line)
         image = pyscreenshot.grab()
-        if (RESULT.blacklisted)
+
+        #if IP blacklisted then write to file
+
+        if (RESULT.blacklisted):
           print(RESULT.blacklisted)
           print(RESULT.detected_by)
-
+          blacklisted_list=(RESULT.detected_by)
+          write_to_file = open("blacklist.txt", "a")
+          write_to_file.writelines("\nblacklisted IP: "+line)
+          write_to_file.write("\nBlacklisted By:")
+          write_to_file.write(json.dumps(blacklisted_list))
+          write_to_file.writelines("\n------------------------")
+          write_to_file.close()
         # To display the captured screenshot
         #image.show()
 
         # To save the screenshot
         image.save(line, 'png')
         print(line.strip())
-
