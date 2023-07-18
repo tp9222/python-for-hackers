e3b48866dc3da470953c083367e4e310e0c5ada2
tp9222 <tp9222@gmail.com>
2023-07-18 19:32:26 +0530

    Update High_Value_Files_Finder(HVFF).bat

--- tools/High_Value_Files_Finder/High_Value_Files_Finder(HVFF).bat
+++ tools/High_Value_Files_Finder/High_Value_Files_Finder(HVFF).bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-set "extensions=.csv .json .xls .xlsx .doc .docx .pdf .ppt .pptx .html .htaccess .properties .env .yml .yaml .py .php .asp .aspx .jsp .war .jar .gz .tar.gz .zip .rar .dbf .ini .rc .log .xml .pem .bak .backup .sql .conf .config .old"
+set "extensions=.csv .json .xls .xlsx .doc .docx .pdf .ppt .pptx .html .htaccess .properties .env .yml .yaml .py .php .asp .aspx .jsp .war .jar .gz .tar.gz .zip .rar .dbf .ini .rc .log .xml .pem .bak .backup .sql .conf .config .pbx .p12 .old"
 
 for %%e in (%extensions%) do (
     dir /b /s "C:\path\to\search\*%%e"
